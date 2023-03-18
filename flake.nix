ce183d2283a3580456c319caf1efd165e414ee91
Joachim Breitner <mail@joachim-breitner.de>
2023-03-18 14:56:34 +0100

    Define docker image

--- flake.nix
+++ flake.nix
@@ -3,9 +3,11 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-22.11";
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
+    ihp.url = github:digitallyinduced/ihp/v1.0.1;
+    ihp.flake = false;
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable } @attrs:
+  outputs = { self, nixpkgs, nixpkgs-unstable, ihp } @attrs:
     let
       system = "x86_64-linux";
       overlay-unstable = final: prev: {
@@ -16,8 +18,39 @@
         #  config.allowUnfree = true;
         # };
       };
+
+    ihpApp = import "${ihp}/NixSupport/default.nix" {
+        ihp = ihp;
+        haskellDeps = p: with p; [
+            cabal-install
+            base
+            wai
+            text
+            hlint
+            p.ihp
+            tz
+            wreq
+            tagsoup
+            modern-uri
+        ];
+        otherDeps = p: with p; [
+            # Native dependencies, e.g. imagemagick
+        ];
+        projectPath = ./.;
+        optimized = true;
+    };
     in
     {
+      packages.${system} = {
+        app = ihpApp;
+        docker = nixpkgs.legacyPackages.${system}.dockerTools.buildImage {
+         name = "hello-docker";
+         config = {
+           Cmd = [ "${ihpApp}/bin/RunProdServer" ];
+         };
+        };
+      };
+
       nixosConfigurations."badjunk" = nixpkgs.lib.nixosSystem {
         inherit system;
         specialArgs = attrs // {
