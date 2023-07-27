30938f150b8f4ef9cf6d1f449e99acbec1f957a1
Joachim Breitner <mail@joachim-breitner.de>
2023-07-27 10:15:17 +0200

    Upgrade to IHP-1.1

--- flake.nix
+++ flake.nix
@@ -1,34 +1,43 @@
 {
-  description = "Shipnix server configuration for badjunk";
-  inputs = {
-    nixpkgs.url = "nixpkgs/nixos-22.11";
-    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
-  };
-
-  outputs = { self, nixpkgs, nixpkgs-unstable } @attrs:
-    let
-      system = "x86_64-linux";
-      overlay-unstable = final: prev: {
-        unstable = nixpkgs-unstable.legacyPackages.${prev.system};
-        # use this variant if unfree packages are needed:
-        # unstable = import nixpkgs-unstable {
-        #  inherit system;
-        #  config.allowUnfree = true;
-        # };
-      };
-    in
-    {
-      nixosConfigurations."badjunk" = nixpkgs.lib.nixosSystem {
-        inherit system;
-        specialArgs = attrs // {
-          environment = "production";
-        };
-        modules = [
-          # Overlays-module makes "pkgs.unstable" available in configuration.nix
-          ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-unstable ]; })
-          ./nixos/configuration.nix
-        ];
-      };
+    inputs = {
+        # Here you can adjust the IHP version of your project
+        # You can find new releases at https://github.com/digitallyinduced/ihp/releases
+        ihp.url = "github:digitallyinduced/ihp/v1.1";
+        nixpkgs.follows = "ihp/nixpkgs";
+        flake-parts.follows = "ihp/flake-parts";
+        devenv.follows = "ihp/devenv";
+        systems.follows = "ihp/systems";
     };
+
+    outputs = inputs@{ ihp, flake-parts, systems, ... }:
+        flake-parts.lib.mkFlake { inherit inputs; } {
+
+            systems = import systems;
+            imports = [ ihp.flakeModules.default ];
+
+            perSystem = { pkgs, ... }: {
+                ihp = {
+                    enable = true;
+                    projectPath = ./.;
+                    packages = with pkgs; [
+                        # Native dependencies, e.g. imagemagick
+                    ];
+                    haskellPackages = p: with p; [
+                        # Haskell dependencies go here
+                        p.ihp
+                        cabal-install
+                        base
+                        wai
+                        text
+                        hlint
+                        # custom deps
+                        tz
+                        wreq
+                        tagsoup
+                        modern-uri
+                    ];
+                };
+            };
+
+        };
 }
-    
